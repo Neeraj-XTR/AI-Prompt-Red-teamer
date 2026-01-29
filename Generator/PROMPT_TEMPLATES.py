c63da3aa5039ce9e8199f422878d93eb31774fac
Neeraj-XTR <107463307+Neeraj-XTR@users.noreply.github.com>
2026-01-29 22:00:59 +0530

    Update PROMPT_TEMPLATES.py

--- Generator/PROMPT_TEMPLATES.py
+++ Generator/PROMPT_TEMPLATES.py
@@ -1,5 +1,6 @@
 PROMPT_TEMPLATES = [
     "{role}. {override}. {scenario} {objective}.",
     "{scenario} {role}. Task: {objective}. {override}",
-    "{role} is asked to perform the following task: {objective}. {scenario}"
+    "{role}. You are asked to perform the following task: {objective}. {scenario}"
 ]
+
